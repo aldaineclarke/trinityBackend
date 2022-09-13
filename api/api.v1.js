411c0ca2f5cb39d3aed954a4b1636c993e321bc2
rackeelBrooks <84402686+cout-Rackeel@users.noreply.github.com>
2022-09-13 14:01:30 -0500

    ticket CRUD complete

--- api/api.v1.js
+++ api/api.v1.js
@@ -1,10 +1,9 @@
 const router = require("express").Router();
-
+const ticketRouter = require('../routes/ticketRoute.js')
 
 // Lists all the resources for the restful api
-
-
-
+router.use('/tickets', ticketRouter);
+router.get('/', (_, res) => res.send('Welcome'));
 
 
 
--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const apiRouter = require('./api/api.v1');
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-app.use(cors());
+app.use(cors('*'));
 
 
 
