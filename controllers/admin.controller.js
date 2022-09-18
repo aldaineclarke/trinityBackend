53c9db9c4ed9298a05c0039a1325d617cb4c7193
aldaine-AG <aldaineclarke1@gmail.com>
2022-09-18 12:16:38 -0500

    fixed error handling in admin and added uploads to gitignore

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -13,6 +13,7 @@ exports.createAdmin = async (req, res) => {
 		req.body.password = await bcrypt.hash(req.body.password, 10);
 		const admin = await Admin.create(req.body);
 		admin.password = undefined;
+		console.log(admin)
 		JSONResponse.success(res, "success", admin, 200);
 	} catch (error) {
 		JSONResponse.error(res, "error", error, 500);
@@ -21,26 +22,24 @@ exports.createAdmin = async (req, res) => {
 
 exports.adminLogin = async (req, res) => {
 	try {
-		const admin = await Admin.findOne({
-			email: req.body.email,
-			username: req.body.username,
-		});
-
-		if (
-			admin &&
-			(await bcrypt.compare(req.body.password, admin.password))
-		) {
-			console.log(admin);
+		const admin = await Admin.findOne({ email: req.body.email});
+		if(admin){
+			let passwordCorrect = await bcrypt.compare(req.body.password, admin.password);
+			if(!passwordCorrect) return JSONResponse.error(res, "error", "Password incorrect", 400)
+	
 			const token = jwt.sign(
-				{ email: admin.email },
-				process.env.JWT_SECRET_KEY
+				{ email: admin.email, username:admin.username, role:"ADMIN" },
+				process.env.JWT_SECRET_KEY,
+				{ 
+					expiresIn: 360
+				}
 			);
 			admin.password = undefined;
 			return JSONResponse.success(res, "success", { token, admin }, 200);
 		} else {
-			JSONResponse.error(res, "error", "Admin does not exist", 500);
+			JSONResponse.error(res, "error","Admin does not exist", 404);
 		}
 	} catch (error) {
-		JSONResponse.error(res, "error", error, 500);
+		JSONResponse.error(res, "error", error, 400);
 	}
 };
